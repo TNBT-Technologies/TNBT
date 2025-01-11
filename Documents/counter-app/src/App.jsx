4224b7af723c612254f53881fd4fe0cef6515eee
anush <anushpoojary0101@gmail.com>
2025-01-11 18:56:29 +0530

    initial commit

---
+++ Documents/counter-app/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from "react";
+
+function App() {
+  const [count, setCount ] = useState(0);
+
+
+  return (
+    <div>
+      <CustomButton count = {count} setCount = {setCount}></CustomButton>
+    </div>
+  )
+}
+  function CustomButton(props){
+
+    function onclickHandler(){
+      props.setCount(props.count + 1)
+    }
+    return <button onClick={onclickHandler}>
+      Counter {props.count}
+    </button>
+  }
+
+
+export default App
